b010e9e52b2c3498fddfde6c142b0ea2e50a26cc
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-04-08 12:55:09 +0200

    first commit again, .gitignore, Makefile and header libft.h

---
+++ libft.h
@@ -0,0 +1,17 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_atoi(const char *str);
+size_t		ft_strlen(const char *s);
+char *		ft_strchr(const char *s, int c);
+
+#endif
