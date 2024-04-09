4645b9015c13bbb363afc5feecdab11087ba4360
Aurelien Focant <afocant@shi-r1-p1.s19.be>
2024-04-09 18:28:55 +0200

    added ft_memcpy and modified libft

---
+++ ft_memcpy.c
@@ -0,0 +1,8 @@
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	while (n--)
+		*((char *) (dst + n)) = *((char *) (src + n));
+	return (dst);
+}
--- libft.h
+++ libft.h
@@ -11,6 +11,9 @@ int		ft_isascii(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_atoi(const char *str);
+void	bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t		ft_strlen(const char *s);
 char *		ft_strchr(const char *s, int c);
 
