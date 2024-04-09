386cbf21ff528014e04f9cb61d03bf83a741bc29
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-04-10 00:22:11 +0200

    finished ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -7,7 +7,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	i = 0;
 	while (len--)
 	{
-		if (dst > src)
+		if (dst < src)
 		{
 			*((char *) (dst + i)) = *((char *) (src + i));
 			i++;
@@ -17,18 +17,3 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	}
 	return (dst);
 }
-
-#include <stdio.h>
-
-int main()
-{
-	int buf[] = {1, 2, 3, 4, 5};
-	int n = 3;
-
-	ft_memmove(buf+1, buf, n);
-	for (int i = 0; i < 5; i++)
-	{
-		printf("%i, ", buf[i]);
-	}	
-}
-
