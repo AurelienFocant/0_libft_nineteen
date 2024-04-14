bff9ebc16affe41496c35b92608da0d39cea8c0f
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-04-14 21:54:32 +0200

    done strmapi

--- libft.h
+++ libft.h
@@ -31,5 +31,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char));
 
 #endif
--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include "libft.h"
 #include <stdio.h>
 
+static char uppercut(unsigned int i, char c)
+{
+	(void) i;
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
 int main()
 {
-	char *s = "  hey  you  mfer";
-	char c = ' ';
-	char **strs;
-	
-	strs = ft_split(s, c);
-	while (*strs)
-		printf("%s\n", *strs++);
-	//printf("%i\n", ft_itoa(472));
+	char *str = "hello bande de cons";
+	char *s;
+
+	char (*f)(unsigned int, char);
+	f = uppercut;
+
+	s = ft_strmapi(str, f);
+
+	printf("%s\n", s);
+
 }
