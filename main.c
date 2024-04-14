f68948000e9024081cd79ef4b587465af4836182
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-04-14 22:47:08 +0200

    done putendl and putnbr

--- main.c
+++ main.c
@@ -3,5 +3,5 @@
 
 int main()
 {
-	ft_putstr_fd("hey you ldjkjajdsfjhasdk fjhsadlkfj hasldkjfh aslkdjfh alksjdhriaosuehrlkja sdnfl kasjdhrp oiuaw priouasd fpiouashdfokiuash edrpiouaheskjhasdlkfjh asldkfb alskdjfp[oawierposauiptiroausdrikuashahjksbdpkausihidrpoiauhropikashdfkasjdhfkjas dhf lkjasdhf paskdhjfj kasdjhf paossuiehirt toaiseut ptoaiushjf ikuasjhgdp ajiw[eopirfj pisauhdf pasdijkfh aopsiuhf alskhjdbf kjashvbf lkasjdnhhb ", 1);
+	ft_putnbr_fd(-483648, 1);
 }
